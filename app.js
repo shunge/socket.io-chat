0f19c66a5ffba02a6e97fbc517e536db661cbc1b
shunge <gyishun@gmail.com>
2016-01-02 23:22:56 -0800

    test

--- app.js
+++ app.js
@@ -3,35 +3,47 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
 app.get('/', function(req, res){
-	res.sendFile(__dirname+'/index.html');
+	res.sendFile(__dirname + '/index.html');
 });
 
-//create new io connection
-io.on('connection',function(socket){
+var user_count = 0;
 
-	//new user
-	socket.on('new user', function(msg){
+//當新的使用者連接進來的時候
+io.on('connection', function(socket){
+
+	//新user
+	socket.on('add user',function(msg){
 		socket.username = msg;
-		io.emit('new user', {
+		console.log("new user:"+msg+" logged.");
+		io.emit('add user',{
 			username: socket.username
 		});
 	});
 
-	//new message
+	//監聽新訊息事件
 	socket.on('chat message', function(msg){
+
+		console.log(socket.username+":"+msg);
+
+  		//發佈新訊息
 		io.emit('chat message', {
-			username: socket.username,
-			msg: msg
+			username:socket.username,
+			msg:msg
 		});
 	});
 
-	//disconnect
-	socket.on('disconnect', function(){
-		console.log('user disconnected');
+	//left
+	socket.on('disconnect',function(){
+		console.log(socket.username+" left.");
+		io.emit('user left',{
+			username:socket.username
+		});
 	});
+
+
 });
 
-//listening to the port
+//指定port
 http.listen(process.env.PORT || 3000, function(){
-  console.log('listening on', http.address().port);
+	console.log('listening on *:3000');
 });
